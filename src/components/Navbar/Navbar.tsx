ac210678bcac8783aeeb3764df3f80bada913b0b
Hemant297 <hemant.khemka66@gmail.com>
2024-08-13 18:01:17 +0530

    Mobile navbar created

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -4,43 +4,50 @@ import { useNavigate } from "react-router-dom";
 import { data } from "./constants";
 import { Button } from "../ui/button";
 import useMediaQuery from "../../hooks/useMediaQuery";
+import MobileNav from "./MobileNav";
 const Navbar = () => {
   const navigate = useNavigate();
   const isMobile = useMediaQuery("(max-width:1000px)");
   return (
-    <div className="flex justify-around text-xl items-center">
-      <img
-        src={Logo}
-        alt="LCE Logo"
-        style={{ cursor: "pointer" }}
-        onClick={() => {
-          navigate("/");
-        }}
-        className={`mt-4 ${
-          isMobile ? "w-[50vw] h-[21vh]" : "w-[15vw] h-[15vh]"
-        }`}
-      />
-      <div className="flex font-semibold text-blue">
-        {data.map((data) => (
-          <div className="mr-8" key={Math.random()}>
-            {data.title === "About" && data.title}
-            {data.title !== "About" && (
-              <Link title={data.title} to={data.route}>
-                {data.title}
-              </Link>
-            )}
+    <>
+      {isMobile ? (
+        <MobileNav />
+      ) : (
+        <div className="flex justify-around text-xl items-center">
+          <img
+            src={Logo}
+            alt="LCE Logo"
+            style={{ cursor: "pointer" }}
+            onClick={() => {
+              navigate("/");
+            }}
+            className={`mt-4 ${
+              isMobile ? "w-[50vw] h-[21vh]" : "w-[15vw] h-[15vh]"
+            }`}
+          />
+          <div className="flex font-semibold text-blue">
+            {data.map((data) => (
+              <div className="mr-8" key={Math.random()}>
+                {data.title === "About" && data.title}
+                {data.title !== "About" && (
+                  <Link title={data.title} to={data.route}>
+                    {data.title}
+                  </Link>
+                )}
+              </div>
+            ))}
           </div>
-        ))}
-      </div>
-      <div className="flex ">
-        <Button variant="ghost" className="text-outlinedButtoncolor">
-          Login
-        </Button>
-        <Button className="bg-gradient-to-r from-orange-500 to-orange-400 ">
-          Sign Up
-        </Button>
-      </div>
-    </div>
+          <div className="flex ">
+            <Button variant="ghost" className="text-outlinedButtoncolor">
+              Login
+            </Button>
+            <Button className="bg-gradient-to-r from-orange-500 to-orange-400 ">
+              Sign Up
+            </Button>
+          </div>
+        </div>
+      )}
+    </>
   );
 };
 
